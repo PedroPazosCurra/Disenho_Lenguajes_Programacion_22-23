dce99ce827e1a949b1a1866d825b853c5591c8ae
Pedro <72791932+PedroPazosCurra@users.noreply.github.com>
2022-12-09 18:35:35 +0100

    lambda.mli: Cambios para contexto global (2.2)

--- Codigo/lambda.mli
+++ Codigo/lambda.mli
@@ -21,17 +21,22 @@ type term =
   | TmAbs of string * ty * term
   | TmApp of term * term
   | TmLetIn of string * term * term
+  | TmFix of term
 ;;
 
-val emptyctx : context;;
-val addbinding : context -> string -> ty -> context;;
-val getbinding : context -> string -> ty;;
+type comando =
+    Eval of term
+  | Bind of string * term 
+
+val emptyctx : 'a context;;
+val addbinding : 'a context -> string -> 'a -> 'a context;;
+val getbinding : 'a context -> string -> 'a;;
 
 val string_of_ty : ty -> string;;
 exception Type_error of string;;
-val typeof : context -> term -> ty;;
+val typeof : ty context -> term -> ty;;
 
 val string_of_term : term -> string;;
 exception NoRuleApplies;;
-val eval : term -> term;;
+val eval : term context -> term -> term;;
 
