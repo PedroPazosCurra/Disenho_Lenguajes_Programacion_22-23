147a8a54d639cec78e85d967fe3e381e6c626800
Pedro <72791932+PedroPazosCurra@users.noreply.github.com>
2022-12-13 14:28:15 +0100

    Update main.ml

--- Codigo/main.ml
+++ Codigo/main.ml
@@ -7,18 +7,20 @@ open Parser;;
 open Lexer;;
 
 
-(* Método top_level_loop: Bucle iterativo superior de la interfaz.                                                 *)
-(*  Como implementación de la funcionalidad de expresiones multilínea (1.1), cada una de las frases que se procesan*)
-(*  son determinadas en base al separador definido ";;".                                                           *)
+(* top_level_loop method: Interface superior iterative loop.                                                       *)
+(*  As an implementation of the multi-line expressions functionality (1.1), each of the sentences being processed  *)
+(*  are determined on the basis of the defined separator ";;".                                                     *)
 (*                                                                                                                 *)
-(*  Para la implementación del contexto de definiciones globales (2.2),                                            *)
-(*  la entrada será pasada ya no directamente al evaluador, sino a la nueva función execute,                       *)
-(*  que determinará si evaluar el término o por el contrario debe ser asignado a un nombre de variable libre.      *)
-(*  Se explora con más detenimiento en el código de su implementación.                                             *)
+(*  For the implementation of the global definition context (2.2), the input will no longer be passed directly 	   *)
+(*  to the evaluator, but to the new execute function, which will determine whether to evaluate the term or whether*)
+(*  it should be assigned to a free variable name. 								                                                 *)
+(*  This is explored in more detail in the code of its implementation.						                                 *)
 (*                                                                                                                 *)
-(*  Durante todo el código posterior se usan para el contexto "vctx" y "tctx". Estas serán nuestras dos listas para*)
-(*  definir el contexto de variables libres y términos, respectivamente. Se usarán a lo largo de toda la           *)
-(*  implementación inferior para jugar con la asociación de variables libres con valores o términos.               *)
+(*  Throughout the subsequent code, "vctx" and "tctx" are used for context.  					                             *)
+(*  These will be our two lists for defining the context of free variables and terms, respectively.                *)
+(*  They will be used throughout the lower implementation to play with 	     					                             *)
+(*  the association of free variables with values or terms.       						                                     *)
+
 
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
