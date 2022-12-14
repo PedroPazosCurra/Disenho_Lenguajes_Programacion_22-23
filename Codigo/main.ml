bc22e6a53322095734db171f540707577ac36fca
Pedro <72791932+PedroPazosCurra@users.noreply.github.com>
2022-12-14 21:46:41 +0100

    comentarios

--- Codigo/main.ml
+++ Codigo/main.ml
@@ -11,25 +11,25 @@ open Lexer;;
 (*  As an implementation of the multi-line expressions functionality (1.1), each of the sentences being processed  *)
 (*  are determined on the basis of the defined separator ";;".                                                     *)
 (*                                                                                                                 *)
-(*  For the implementation of the global definition context (2.2), the input will no longer be passed directly *)
+(*  For the implementation of the global definition context (2.2), the input will no longer be passed directly     *)
 (*  to the evaluator, but to the new execute function, which will determine whether to evaluate the term or whether*)
-(*  it should be assigned to a free variable name. 								                                                 *)
-(*  This is explored in more detail in the code of its implementation.						                                 *)
+(*  it should be assigned to a free variable name. 								   *)
+(*  This is explored in more detail in the code of its implementation.						   *)
 (*                                                                                                                 *)
-(*  Throughout the subsequent code, "vctx" and "tctx" are used for context.  					                             *)
+(*  Throughout the subsequent code, "vctx" and "tctx" are used for context.  					   *)
 (*  These will be our two lists for defining the context of free variables and terms, respectively.                *)
-(*  They will be used throughout the lower implementation to play with 	     					                             *)
-(*  the association of free variables with values or terms.       						                                     *)
-(*															*)
-(* file_eating_loop has been added as a new functionality for IO operations (2.10).                                 *)
-(*  The core of this new method is very similar to top_level_loop but it instead opens, reads and closes a file from parameter*)
-(*  Besides, top_level_loop has been abstracted into a new method that differentiates the now two different executable files*)
-(*   that are created on compilation. "top" works as usual while "run" takes a file as parameter and processes it*)
+(*  They will be used throughout the lower implementation to play with 	     					   *)
+(*  the association of free variables with values or terms.       						   *)
+(*													 	   *)
+(* file_eating_loop has been added as a new functionality for IO operations (2.10).                                *)
+(*  The core of this new method is very similar to top_level_loop but it instead opens, reads and closes a file from parameter *)
+(*  Besides, top_level_loop has been abstracted into a new method that differentiates the now two different executable files   *)
+(*   that are created on compilation. "top" works as usual while "run" takes a file as parameter and processes it  *)
 
 
 
 (* Note: We are aware that "file_eating_loop" method isn't working as it should. But it was decided to be kept in the file due *)
-(*        to it being innocuous to the rest of the program. *)
+(*        to it being innocuous to the rest of the program.                                                                    *)
 
 exception Invalid_input of string;;
 
